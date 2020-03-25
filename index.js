789d5f716f0bba3acd39d71c1569fa047daa7b6d
javibotella <javi.botella.r@gmail.com>
2020-03-25 10:20:52 +0000

    problema con responde resuelto

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 80;
 
 app.use("/", express.static("./public"));
 app.get("/cool", (request, response)=>{
-	responde.send("<html>"+cool()+"</html>");
+	response.send("<html>"+cool()+"</html>");
 });
 app.listen(port, ()=> {
 	console.log("Server ready");
