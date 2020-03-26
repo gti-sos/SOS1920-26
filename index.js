200bbd648d46bedf3939a6158792ad340876f992
jgcreus <jgcreus@outlook.es>
2020-03-26 13:20:22 +0000

    intento03

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var coef = [
 	{ 
 		country: "spain",
 		year: 2018,
-		team: "real madrid",
+		team: "real-madrid",
 		coefficient: 146.000,
 		fed: 20.713,
 		classification: 1
@@ -27,7 +27,7 @@ var coef = [
 	{ 
 		country: "spain",
 		year: 2016,
-		team: "atletico de madrid",
+		team: "atletico-de-madrid",
 		coefficient: 122.000,
 		fed: 20.999,
 		classification: 4
@@ -53,6 +53,49 @@ var coef = [
 const BASE_API_URL = "/api/v1";
 
 
+//LOADINITIALDATA
+app.get(BASE_API_URL + "/coef/loadInitialData", (req, res) => {
+
+	coef = [
+	{ country: "spain",
+		year: 2018,
+		team: "real-madrid",
+		coefficient: 146.000,
+		fed: 20.713,
+		classification: 1
+	}, { country: "germany",
+		year: 2018,
+		team: "bayern",
+		coefficient: 128.000,
+		fed: 14.385,
+		classification: 3
+	}, { country: "spain",
+		year: 2016,
+		team: "atletico-de-madrid",
+		coefficient: 122.000,
+		fed: 20.999,
+		classification: 4
+	}, { country: "italy",
+		year: 2018,
+		team: "juventus",
+		coefficient: 124.000,
+		fed: 14.945,
+		classification: 5}, { country: "spain",
+		year: 2018,
+		team: "sevilla",
+		coefficient: 104.000,
+		fed: 20.713,
+		classification: 7}];
+	
+	
+		res.send(JSON.stringify(coef,null,2));
+		res.sendStatus(200);
+});
+
+
+
+
+
 // GET coef
 
 app.get(BASE_API_URL+"/coef", (req,res) =>{
@@ -65,27 +108,34 @@ app.get(BASE_API_URL+"/coef", (req,res) =>{
 
 app.post(BASE_API_URL+"/coef",(req,res) =>{
 	
-	var newTeam = req.body;
+	var newCoef = req.body;
 	
-	if((newTeam == "") || (newTeam.team == null)){
+	if((newCoef == "") || (newCoef.team == null)){
 		res.sendStatus(400,"BAD REQUEST");
 	} else {
-		coef.push(newTeam); 	
+		coef.push(newCoef); 	
 		res.sendStatus(201,"CREATED");
 	}
 });
 
-// DELETE CONTACTS
+// DELETE coef
+app.delete(BASE_API_URL + "/coef", (req,res)=>{
+	
+	coef = [];
+	
+	res.sendStatus(200);
+});
 
-// GET CONTACT/XXX
+// GET coef/team/year
 
-app.get(BASE_API_URL+"/coef/:team", (req,res)=>{
+app.get(BASE_API_URL+"/coef/:team/:year", (req,res)=>{
 	
 	var team = req.params.team;
+	var year = req.params.year;
 	
-	var filteredCoef = coef.filter((c) => {
-		return (c.team == team);
-	});
+ filteredCoef = coef.filter((c) => {
+    return (c.team == team && c.year ==year);
+  });
 	
 	
 	if(filteredCoef.length >= 1){
@@ -95,17 +145,55 @@ app.get(BASE_API_URL+"/coef/:team", (req,res)=>{
 	}
 });
 
-// PUT CONTACT/XXX
+// update coef/team/year
 
-// DELETE CONTACT/XXX
+app.put(BASE_API_URL+"/coef/:team/:year", (req, res) =>{
+  
+  var team = req.params.team;
+  var year = req.params.year;
+	var updateCoef = req.body;
+  
+  filteredCoef = coef.filter((c) => {
+    return (c.team == team && c.year ==year);
+  });
 
-app.delete(BASE_API_URL+"/coef/:team", (req,res)=>{
+  if(filteredCoef.length == 0){
+    res.sendStatus(404);
+    return;
+  }
+  
+  if(!updateCoef.country || !updateCoef.year ||!updateCoef.coefficient || !updateCoef.fed
+     || !updateCoef.classification || updateCoef.team != team){
+                console.log("COEF  FIND. NO FACT VALID 400");
+                res.sendStatus(400);
+    return;
+  }
+  
+  coef = coef.map((c) => {
+    if(c.team == updateCoef.team){
+      return updateCoef;
+    }else{
+      return c;
+    }
+    
+  });
+  res.sendStatus(200);
+});
+
+
+
+
+
+// DELETE CONTACT/team/year
+
+app.delete(BASE_API_URL+"/coef/:team/:year", (req,res)=>{
 	
 	var team = req.params.team;
+	var year = req.params.year;
 	
 	var filteredCoef = coef.filter((c) => {
-		return (c.team != team);
-	});
+    return (c.team == team && c.year ==year);
+ 	 });
 	
 	
 	if(filteredCoef.length < coef.length){
@@ -118,6 +206,16 @@ app.delete(BASE_API_URL+"/coef/:team", (req,res)=>{
 	
 });
 
+//POST error
+app.post(BASE_API_URL + "/coef/:team/:year", (req, res) => {
+    res.sendStatus(405);
+});
+
+//PUT error
+app.put(BASE_API_URL + "/coef", (req, res) => {
+    res.sendStatus(405);
+});
+
 
 app.listen(port, () => {
 	console.log("Server ready");
@@ -127,23 +225,4 @@ console.log("Starting server...");
 
 
 
-
-
-
-/*
-app.get(BASE_API_URL+"/coef", (req,res) =>{
-	res.send(JSON.stringify(coef,null,2));
-});
-
-app.post(BASE_API_URL+"/coef",(req,res) =>{
-	coef.push(req.body); 
-	res.sendStatus(201,"CREATED");
-});
-
-app.listen(port, () => {
-	console.log("Server ready");
-});
-
-console.log("Starting server...");
-*/
 
