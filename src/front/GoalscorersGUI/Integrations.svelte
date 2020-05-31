1df698ab007d6285431038c2303cf25463b7268b
Javier Botella <50969124+MrBotella@users.noreply.github.com>
2020-05-31 12:00:56 +0200

    menu de integraciones

--- src/front/GoalscorersGUI/Integrations.svelte
+++ src/front/GoalscorersGUI/Integrations.svelte
@@ -7,9 +7,14 @@
     <h2 style="text-align: center;">Integraciones de Goalscorers</h2>
     <br><br>   
     <h3 style="text-align: center;">Integraciones de SOS</h3> 
-    <button type="button" class="btn btn-outline-info" onclick="window.location.href='#/integrations/global-transfers/integrationsG8'" style="margin-left: 12.5%; width: 25%;"> Integración con Grupo 8</button>
-	<button type="button" class="btn btn-outline-success" onclick="window.location.href='#/integrations/global-transfers/integrationsG9'" style="width: 25%;"> Integración con Grupo 9</button>
-	<button type="button" class="btn btn-outline-danger" onclick="window.location.href='#/integrations/global-transfers/integrationsG10'" style= "width: 25%;"> Integración con Grupo 10 </button><br><br>
+    <div align="center">
+    <button type="button" class="btn btn-outline-info" onclick="window.location.href='#/integrations/goalscorers/integrationG10'" style= "width: 20%;"> Integración con Grupo 10 </button>
+    <button type="button" class="btn btn-outline-info" onclick="window.location.href='#/integrations/goalscorers/integrationG12'" style= "width: 20%;"> Integración con Grupo 12 </button>
+    <button type="button" class="btn btn-outline-info" onclick="window.location.href='#/integrations/goalscorers/integrationG21'" style="width: 20%;"> Integración con Grupo 21</button>
+    <button type="button" class="btn btn-outline-info" onclick="window.location.href='#/integrations/goalscorers/integrationG22'" style="width: 20%;"> Integración con Grupo 22</button>    
+    </div>
+    
+    <br><br>
 
     <Button outline color="secondary" on:click="{pop}"> <i class="fas fa-arrow-circle-left"></i> Atrás </Button>
 
