1fdf73e435cb3352caac92bac2dd9ec6bbf4175c
jgcreus <jgcreus@outlook.es>
2020-06-01 22:12:43 +0200

    no me lo puedo creer

--- src/front/CoefGUI/GraphCoef2.svelte
+++ src/front/CoefGUI/GraphCoef2.svelte
@@ -1,13 +1,8 @@
-
-
-<html lang="EN">
-    <div id="piechart"></div>
-    
-    </html>
-
 <script>
     google.charts.load('current', { 'packages': ['corechart'] });
     google.charts.setOnLoadCallback(drawChart);
+    import { pop } from "svelte-spa-router";
+    import Button from "sveltestrap/src/Button.svelte";
 
     async function drawChart() {
 
@@ -17,15 +12,12 @@
         let MyData = await resData.json();
         console.log(MyData)
 
-        /* Getting the countries */
-        let countries = Array.from(new Set(MyData.map((d) => { return d.country; })));
-        /* Mapping the data in the right format */
-       
+
         MyData = await MyData.map((d) => {
-            return [d.country, d.coefficient, d.year];
+            return [d.team, d.fed, d.coefficient];
         });
 
-        let DataTypes = [["Pais", "coefficient", "Year"]]
+        let DataTypes = [["Equipo", "Clasificación", "Coeficiente"]]
 
         Array.prototype.push.apply(DataTypes, MyData)
 
@@ -35,7 +27,7 @@
         var data = google.visualization.arrayToDataTable(DataTypes);
 
         var options = {
-            title: 'Matrimonios registrados a nivel global'
+            title: 'Equipos y feds del top 10 en el ranking UEFA'
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
@@ -43,3 +35,13 @@
         chart.draw(data, options);
     }
 </script>
+
+<main>
+    <Button outline color="secondary" on:click="{pop}"> <i class="fas fa-arrow-circle-left"></i> Atrás </Button>
+</main>
+
+<html lang="EN">
+<div id="piechart"></div>
+
+
+</html>
