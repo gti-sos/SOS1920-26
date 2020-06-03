c7b5bf85f0b39551deecc95d54c5c80a336b633c
diemonvaz <diemonvaz@alum.us.es>
2020-06-04 00:46:35 +0200

    fin de vd

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -6,21 +6,35 @@
 <main>
     <h2 style="text-align: center;"><i class="fa fa-video-camera" aria-hidden="true"></i> Video de API</h2>
     <br><br>   
-<div class="row">
-    <div class="card">
-        <h4 class="card-title" style="text-align: center; color: grey; margin-top: 10px; margin-bottom: 10px;"> Mercado de fichajes </h4>
-        <div class="embed-responsive embed-responsive-16by9">
-        <iframe width="420" height="315"
-    src="https://www.youtube.com/embed/tgbNymZ7vqY?controls=0">
-    </iframe>
+    <div class="card-deck">
+        <div class="card">
+            <h4 class="card-title" style="text-align: center; color: grey; margin-top: 10px; margin-bottom: 10px;"> API Mercado de fichajes </h4>
+            <div class="embed-responsive embed-responsive-16by9 video">
+                <iframe width="420" height="315" src="https://www.youtube.com/embed/QerpS9m8a9Q" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <h6 class = "card-body" style="color: grey; margin-top: 10px; margin-bottom: 10px;"> Explicación funcionamiento Transfer API </h6>
         </div>
-    <h6 class = "card-body" style="color: grey; margin-top: 10px; margin-bottom: 10px;"> Explicación funcionamiento Transfer API </h6>
-    
-    </div>
-    
+
+        <div class="card">
+            <h4 class="card-title" style="text-align: center; color: grey; margin-top: 10px; margin-bottom: 10px;"> API Coeficiente UEFA </h4>
+            <div class="embed-responsive embed-responsive-16by9 video">
+                <iframe width="420" height="315" src="https://www.youtube.com/embed/2ZV7mNoiUxo" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <h6 class = "card-body" style="color: grey; margin-top: 10px; margin-bottom: 10px;"> Explicación funcionamiento COEF API </h6>
+        </div>
+
+        <div class="card">
+            <h4 class="card-title" style="text-align: center; color: grey; margin-top: 10px; margin-bottom: 10px;"> API Goleadores </h4>
+            <div class="embed-responsive embed-responsive-16by9 video">
+                <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <h6 class = "card-body" style="color: grey; margin-top: 10px; margin-bottom: 10px;"> Explicación funcionamiento GoalScorers API </h6>
+        </div>
+
+
+    <!--Aqui van los vuestros; dentro de card deck-->
+
 </div>
-
-
 <div class="row">
     <hr>
 </div>
